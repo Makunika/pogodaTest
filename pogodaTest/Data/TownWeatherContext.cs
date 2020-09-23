52ecbcc64a59e6af64f1a17f353abeb63cf45655
Максим Пшибло <pshblo.max@gmail.com>
2020-09-24 01:54:16 +0300

    Готово??

--- pogodaTest/Data/TownWeatherContext.cs
+++ pogodaTest/Data/TownWeatherContext.cs
@@ -5,6 +5,24 @@ namespace pogodaTest.Data
 {
     public class TownWeatherContext : DbContext
     {
+        /* SQL скрипт для развертывания
+         
+            CREATE TABLE Towns
+            (
+                TownId INT Primary KEY,
+                TownName VARCHAR(100) NOT NULL
+            );
+
+            CREATE TABLE Weathers
+            (
+                WeatherId INT Primary KEY,
+                TownId INT NOT NULL,
+                Degree INT NOT NULL,
+                About VARCHAR(100) NOT NULL,
+                WeatherDateTime DATETIME,
+                CONSTRAINT tw_fk FOREIGN KEY (TownId) REFERENCES Towns(TownId)
+            );
+         */
 
         public DbSet<Town> Towns { get; set; }
         public DbSet<Weather> Weathers { get; set; }
